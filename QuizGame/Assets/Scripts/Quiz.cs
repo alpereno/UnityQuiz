1fa17d6accdabfa877d2e143c63392ca221c678e
Alperen Onal <alperenonal9922@gmail.com>
2022-08-10 20:11:55 +0300

    Timer implemented & Code Arrangement
    + Timer Script included
    + on time is up calling wait time to show answer
    + Calling next question on time is up and after answered question

--- QuizGame/Assets/Scripts/Quiz.cs
+++ QuizGame/Assets/Scripts/Quiz.cs
@@ -6,56 +6,86 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI questionText;    
-    [SerializeField] QuestionSO question;
+    public event System.Action OnAnswered;
+    [Header("Questions")]
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
+    QuestionSO currentQuestion;
+
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
-    int correctAnswerIndex;
+
+    [Header("Button Colors")]
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
 
+    //Timer timer;
     int answersLength;
+    int correctAnswerIndex;
+    bool hasAnswered;
 
+    Timer timer;
 
     private void Start()
     {
-        //GetNextQuestion();
-        DisplayQuestion();
+        timer = FindObjectOfType<Timer>();
+        if (timer != null)
+        {
+            timer.OnTimeIsUp += OnTimeIsUp;
+            timer.OnNextQuestion += OnNextQuestion;
+        }
+        NextQuestion();
+    }
+
+    //private void Update()
+    //{
+    //    timerImage.fillAmount = timer.percent;
+    //    if (timer.nextQuestion)
+    //    {
+    //        hasAnswered = false;
+    //        GetNextQuestion();
+    //        timer.nextQuestion = true;
+    //    }
+    //    else if (!hasAnswered && !timer.answeringQuestion)
+    //    {
+    //        DisplayAnswer(-1);
+    //        //SetButtonState(false);
+    //    }
+    //}
+
+    void NextQuestion()
+    {
+        if (questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            GetRandomQuestion();
+            DisplayQuestion();
+        }
+        // else call an event game is over congrt. etc.
+        // time stop
     }
 
     void DisplayQuestion()
     {
         answersLength = answerButtons.Length;
-        questionText.text = question.GetQuestion();
+        questionText.text = currentQuestion.GetQuestion();
         for (int i = 0; i < answersLength; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
+            buttonText.text = currentQuestion.GetAnswer(i);
         }
     }
 
-    void GetNextQuestion()
+    void GetRandomQuestion()
     {
-        SetButtonState(true);
-        DisplayQuestion();
-    }
+        int randomNumber = Random.Range(0, questions.Count);
+        currentQuestion = questions[randomNumber];
 
-    // connect this method to all of answers. Need to know wich button pressed so with index variable
-    public void OnAnswerSelected(int index)
-    {
-        correctAnswerIndex = question.GetCorrectAnswerIndex();
-        Image correctAnswerButtonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
-
-        if (index == correctAnswerIndex)
+        if (questions.Contains(currentQuestion))
         {
-            questionText.text = "Correct!";
+            questions.Remove(currentQuestion);
         }
-        else
-        {
-            string correctAnswer = question.GetAnswer(correctAnswerIndex);
-            questionText.text = "Sorry, the correct answer was; \n" + correctAnswer;
-        }
-        correctAnswerButtonImage.sprite = correctAnswerSprite;
-        SetButtonState(false);
     }
 
     void SetButtonState(bool state)
@@ -69,7 +99,54 @@ public class Quiz : MonoBehaviour
 
     void SetDefaultButtonSprites()
     {
-        Image changedButtonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
-        changedButtonImage.sprite = defaultAnswerSprite;        
+        //for (int i = 0; i < answersLength; i++)
+        //{
+        //    Image changedButtonImage = answerButtons[i].GetComponent<Image>();
+        //    changedButtonImage.sprite = defaultAnswerSprite;
+        //}
+            Image changedButtonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            changedButtonImage.sprite = defaultAnswerSprite;
+    }
+
+    // connect this method to all of answers. Need to know wich button pressed so with index variable
+    public void OnAnswerSelected(int index)
+    {
+        hasAnswered = true;
+        DisplayAnswer(index);
+        SetButtonState(false);
+        if (OnAnswered != null)
+        {
+            OnAnswered();
+        }
+    }
+
+    void DisplayAnswer(int index)
+    {
+        correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+        Image correctAnswerButtonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct!";
+        }
+        else
+        {
+            string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was; \n" + correctAnswer;
+        }
+        correctAnswerButtonImage.sprite = correctAnswerSprite;
+        SetButtonState(false);
+    }
+
+    void OnTimeIsUp()
+    {
+        print("Time is up for answering!!!");
+        DisplayAnswer(-1);
+    }
+
+    void OnNextQuestion()
+    {
+        print("Answer Showing time is over!!!");
+        NextQuestion();
     }
 }
---
+++ QuizGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public event System.Action OnTimeIsUp;
+    public event System.Action OnNextQuestion;
+    // the variable that showing player's answering phase
+    public bool answeringQuestion;
+    public float percent;
+    public bool nextQuestion;
+
+    [SerializeField] Image timerImage;
+    [SerializeField] float fullTime = 30;
+    [SerializeField] float timeToShowCorrectAnswer = 10;
+    
+    float timerValue;
+
+    private void Start()
+    {
+        FindObjectOfType<Quiz>().OnAnswered += OnAnsweredQuestion;
+        answeringQuestion = true;
+        timerValue = fullTime;
+    }
+
+    private void Update()
+    {
+        Countdown();
+    }
+
+    //public void CancelTimer()
+    //{
+    //    timerValue = 0;
+    //}
+
+    void Countdown()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (answeringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                percent = timerValue / fullTime;
+                timerImage.fillAmount = percent;
+            }
+            else
+            {
+                // this line should run 1 time too
+                // call an event for time is up for answering question to show answering 
+                answeringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+
+                if (OnTimeIsUp != null)
+                {
+                    OnTimeIsUp();
+                }
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                percent = timerValue / timeToShowCorrectAnswer;
+                timerImage.fillAmount = percent;
+            }
+            else
+            {
+                answeringQuestion = true;
+                timerValue = fullTime;
+                //nextQuestion = true;
+                // this line should run 1 time
+                // call an event time is up for showing answer and call the next question
+                if (OnNextQuestion != null)
+                {
+                    OnNextQuestion();
+                }
+            }
+        }
+        //print(answeringQuestion + " : " + timerValue + " = " + percent);
+    }
+
+    void OnAnsweredQuestion()
+    {
+        answeringQuestion = false;
+        timerValue = timeToShowCorrectAnswer;
+    }
+}
